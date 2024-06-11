02101309dd23fc62830f22e1d760a5dd032ff934
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-06-10 21:57:13 -0300

    fix: Erro de criação e de edição de contatos na agenda

--- app/controllers/AgendaController.js
+++ app/controllers/AgendaController.js
@@ -1,41 +1,49 @@
-const Agenda  = require('../models/Agenda');
+const Agenda = require("../models/Agenda");
 const AgendaRepository = require("../repositories/AgendaRepository");
 
 function AgendaController() {
-
   async function list(req, res) {
-    const contatos = await AgendaRepository.getAll;
+    const contatos = await AgendaRepository.getAll();
 
-    res.render('/agenda/list', { 
+    res.render("agenda/list", {
       title: "Lista de Contatos",
-      contatos: contatos
-    })
+      contatos: contatos,
+    });
   }
 
   function create(req, res) {
-    res.render('/agenda/create')
+    res.render("agenda/create");
   }
 
   async function save(req, res) {
-    await AgendaRepository.update(req.body);
-    res.redirect('/');
+    try {
+      if (!req.body) {
+        throw new Error("Dados do formulário não foram enviados");
+      }
+      await AgendaRepository.create(req.body);
+      res.redirect("/agenda");
+    } catch (error) {
+      console.error("Erro ao salvar o contato:", error.message);
+      res
+        .status(500)
+        .send("Erro ao salvar o contato. Por favor, tente novamente.");
+    }
   }
 
   async function remove(req, res) {
     await AgendaRepository.remove(req.params.id);
-    res.redirect('/agenda')
+    res.redirect("/agenda");
   }
 
   async function edit(req, res) {
-    const contato = await AgendaRepository.find(req.params.id);
-    res.render('agenda/edit', { contato: contato })
+    const contato = await AgendaRepository.findByID(req.params.id);
+    res.render("agenda/edit", { contato: contato });
   }
 
   async function update(req, res) {
-    await tasksRepository.update(req.body.id, req.body);
-    res.redirect('/agenda');
+    await AgendaRepository.update(req.body.id, req.body);
+    res.redirect("/agenda");
   }
-
 
   return {
     create,
@@ -44,8 +52,7 @@ function AgendaController() {
     remove,
     edit,
     update,
-  }
-
+  };
 }
 
 module.exports = AgendaController();
